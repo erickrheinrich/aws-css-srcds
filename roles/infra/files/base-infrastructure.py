967e61a4cac85cbed0fc3609505254b3eb99b150
erickrheinrich <erick@heinrich.com.br>
2017-12-04 13:38:05 -0200

    Simplifying infrastructure

--- roles/infra/files/base-infrastructure.py
+++ roles/infra/files/base-infrastructure.py
@@ -1,7 +1,6 @@
-from troposphere import Template, Parameter, Ref, Base64, Join, Tags, GetAtt, Output, Select
-from troposphere.ec2 import VPC, Subnet, NatGateway, InternetGateway, RouteTable, Route, VPCGatewayAttachment, \
-    SubnetRouteTableAssociation, EIP, SecurityGroup, SecurityGroupRule, NetworkInterfaceProperty, Instance, NetworkAcl, \
-    NetworkAclEntry, PortRange, SubnetNetworkAclAssociation, Tag
+from troposphere import Template, Ref, Join, Tags, Output
+from troposphere.ec2 import VPC, Subnet, InternetGateway, RouteTable, Route, VPCGatewayAttachment, \
+    SubnetRouteTableAssociation
 
 
 def generate_template():
@@ -14,18 +13,6 @@ def generate_template():
     template.add_description(
         'Base infrastructure Stack implementing VPC Scenario 2 with 2 private subnets'
     )
-
-    environment_name = template.add_parameter(Parameter(
-        'EnvName',
-        Type='String',
-        Description='The name of the environment being deployed.'
-    ))
-
-    aws_region = template.add_parameter(Parameter(
-        'AWSRegion',
-        Type='String',
-        Description='The name of the region the environment should be deployed.'
-    ))
 
     # Create VPC
 
@@ -43,7 +30,7 @@ def generate_template():
             Tags=Tags(
                 Application=ref_stack_id)))
 
-    internet_gateway_attachment = template.add_resource(
+    template.add_resource(
         VPCGatewayAttachment(
             'AttachGateway',
             VpcId=Ref(vpc),
@@ -57,7 +44,7 @@ def generate_template():
             Tags=Tags(
                 Application=ref_stack_id)))
 
-    route = template.add_resource(
+    template.add_resource(
         Route(
             'Route',
             DependsOn='AttachGateway',
@@ -66,128 +53,24 @@ def generate_template():
             RouteTableId=Ref(public_route_table),
         ))
 
-    # Create NAT Gateway for Public Subnet
-
-    nat_eip1 = template.add_resource(EIP(
-        'NatEip1',
-        Domain="vpc",
-    ))
-
-    nat_eip2 = template.add_resource(EIP(
-        'NatEip',
-        Domain="vpc",
-    ))
-
     # Create Public Subnet
     public_subnet1 = template.add_resource(
         Subnet(
             'PublicSubnet1',
             CidrBlock='10.0.0.0/24',
-            AvailabilityZone=Join("", [Ref(aws_region), 'a']),
+            AvailabilityZone=Join("", [ref_region, 'a']),
             MapPublicIpOnLaunch=True,
             VpcId=Ref(vpc),
             Tags=Tags(
                 Name='public-10.0.0.0',
                 Application=ref_stack_id)))
 
-    public_subnet2 = template.add_resource(
-        Subnet(
-            'PublicSubnet2',
-            CidrBlock='10.0.3.0/24',
-            AvailabilityZone=Join("", [Ref(aws_region), 'b']),
-            MapPublicIpOnLaunch=True,
-            VpcId=Ref(vpc),
-            Tags=Tags(
-                Name='public-10.0.3.0',
-                Application=ref_stack_id)))
-
-    subnetRouteTableAssociation1 = template.add_resource(
+    template.add_resource(
         SubnetRouteTableAssociation(
             'SubnetRouteTableAssociation1',
             SubnetId=Ref(public_subnet1),
             RouteTableId=Ref(public_route_table),
         ))
-
-    subnetRouteTableAssociation2 = template.add_resource(
-        SubnetRouteTableAssociation(
-            'SubnetRouteTableAssociation2',
-            SubnetId=Ref(public_subnet2),
-            RouteTableId=Ref(public_route_table),
-        ))
-
-    nat1 = template.add_resource(NatGateway(
-        'Nat1',
-        AllocationId=GetAtt(nat_eip1, 'AllocationId'),
-        SubnetId=Ref(public_subnet1),
-    ))
-
-    nat2 = template.add_resource(NatGateway(
-        'Nat2',
-        AllocationId=GetAtt(nat_eip2, 'AllocationId'),
-        SubnetId=Ref(public_subnet2),
-    ))
-
-    private_subnet1 = template.add_resource(
-        Subnet(
-            'PrivateSubnet1',
-            CidrBlock='10.0.1.0/24',
-            AvailabilityZone=Join("", [Ref(aws_region), 'a']),
-            MapPublicIpOnLaunch=False,
-            VpcId=Ref(vpc),
-            Tags=Tags(
-                Name='private-az1-10.0.1.0',
-                Application=ref_stack_id)))
-
-    private_subnet2 = template.add_resource(
-        Subnet(
-            'PrivateSubnet2',
-            CidrBlock='10.0.2.0/24',
-            AvailabilityZone=Join("", [Ref(aws_region), 'b']),
-            MapPublicIpOnLaunch=False,
-            VpcId=Ref(vpc),
-            Tags=Tags(
-                Name='private-az2-10.0.2.0',
-                Application=ref_stack_id)))
-
-    private_route_table1 = template.add_resource(
-        RouteTable(
-            'PrivateRouteTable1',
-            VpcId=Ref(vpc),
-            Tags=Tags(
-                Application=ref_stack_id)))
-
-    private_route_table2 = template.add_resource(
-        RouteTable(
-            'PrivateRouteTable2',
-            VpcId=Ref(vpc),
-            Tags=Tags(
-                Application=ref_stack_id)))
-
-    template.add_resource(
-        Route(
-            'NatRoute1',
-            RouteTableId=Ref(private_route_table1),
-            DestinationCidrBlock='0.0.0.0/0',
-            NatGatewayId=Ref(nat1), ))
-
-    template.add_resource(
-        Route(
-            'NatRoute2',
-            RouteTableId=Ref(private_route_table2),
-            DestinationCidrBlock='0.0.0.0/0',
-            NatGatewayId=Ref(nat2), ))
-
-    template.add_resource(
-        SubnetRouteTableAssociation(
-            'PrivateSubnetRouteTableAssociation1',
-            SubnetId=Ref(private_subnet1),
-            RouteTableId=Ref(private_route_table1), ))
-
-    template.add_resource(
-        SubnetRouteTableAssociation(
-            'PrivateSubnetRouteTableAssociation2',
-            SubnetId=Ref(private_subnet2),
-            RouteTableId=Ref(private_route_table2), ))
 
     # Outputs
 
@@ -204,30 +87,13 @@ def generate_template():
     ))
 
     template.add_output(Output(
-        'PublicSubnet2',
-        Value=Ref(public_subnet2),
-        Description='Public subnet ID'
-    ))
-
-    template.add_output(Output(
-        'PrivateSubnet1',
-        Value=Ref(private_subnet1),
-        Description='Private subnet ID'
-    ))
-
-    template.add_output(Output(
-        'PrivateSubnet2',
-        Value=Ref(private_subnet2),
-        Description='Private subnet ID'
-    ))
-
-    template.add_output(Output(
         'StackID',
-        Value=Ref('AWS::StackName'),
+        Value=ref_stack_name,
         Description='Stack ID'
     ))
 
     return template
+
 
 if __name__ == '__main__':
     print (generate_template().to_json())
